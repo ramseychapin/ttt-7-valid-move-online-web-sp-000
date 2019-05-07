122d6648fdcd908e534fc26cebfa2757d03ed8e3
Ramsey Chapin <ramsey@ramseychapin.com>
2019-05-07 01:26:42 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -12,7 +12,7 @@ end
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     return false
-  else board[index] == "X" || board[index] == "O"
+  else
     return true
   end
 end
