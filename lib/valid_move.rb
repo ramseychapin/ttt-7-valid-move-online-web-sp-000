49476b228d3c187b3d57480cb72b65c2a90372d7
Ramsey Chapin <ramsey@ramseychapin.com>
2019-05-07 01:30:41 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(board, position)
   array_spot = position.to_i - 1
-  if array_spot.between?(0,8) && !position_taken?(board, position)
+  if position.between?(0,8) && !position_taken?(board, position)
     return true
   else
     return false
